5a1e0f39b743a1264a3c6ddf9e6a673e800cbdd1
xiayimin <xiayimin@sunlands.com>
2017-06-30 20:21:13 +0800

    新增两个包：Test,LittleGames

---
+++ com/sunlands/xiayimin/LittleGames/KillGame.java
@@ -0,0 +1,9 @@
+package com.sunlands.xiayimin.LittleGames;
+
+/**
+ * Created by sunlandsxym on 2017/6/30.
+ * 500个小孩手拉手排成一圈，从第一个小孩开始，没数三个，干死一个，问当只剩下最后一个小孩的编号是多少
+ *
+ */
+public class KillGame {
+}
---
+++ com/sunlands/xiayimin/Test/Test1.java
@@ -0,0 +1,10 @@
+package com.sunlands.xiayimin.Test;
+
+/**
+ * Created by sunlandsxym on 2017/6/30.
+ */
+public class Test1 {
+    public static void main(String[] args){
+        System.out.println("测试函数包");
+    }
+}
